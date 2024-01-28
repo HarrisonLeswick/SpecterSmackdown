a20ff30fb9e4e5e6a2f4fa6c3ff19cd857a630eb
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2024-01-28 13:30:05 -0500

    Full redraw functionality unweighted

--- Specter Smackdown/Assets/Scripts/cardRegen.cs
+++ Specter Smackdown/Assets/Scripts/cardRegen.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems; 
 
-public class cardRegen : MonoBehaviour
+public class cardRegen : MonoBehaviour, IPointerClickHandler
 {
+    public GameObject cardPile;
+    public int slot;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,18 @@ public class cardRegen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        gameObject.SetActive(false);
+        gameObject.transform.localScale = new Vector3(1.5f, 2.0f, 1.0f);;
+        cardPile.GetComponent<cardPile>().SlideDown(slot);
+
+
+    }
+
+    public void NewSlot(int i){
+        slot = i;
     }
 }
